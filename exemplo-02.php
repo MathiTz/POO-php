056e91f08505411cc51ce50cd75436dfa2e5d309
mathitz <matheusalves789@gmail.com>
2018-09-26 08:49:01 -0300

    Uso de função com método get e set, e como setar o tipo de variável

---
+++ exemplo-02.php
@@ -0,0 +1,62 @@
+<?php   
+
+
+class Carro{
+
+    private $modelo;
+    private $motor;
+    private $ano;
+
+    public function getModelo(){
+
+        return $this->modelo;
+
+    }
+
+    public function setModelo($modelo){
+
+        $this->modelo = $modelo;
+
+    }
+
+    public function getMotor():float{ //dois pontos para setar o tipo de variável
+
+        return $this->motor;
+    }
+
+    public function setMotor($motor){
+
+        $this->motor = $motor;
+    }
+
+    public function getAno():int{ // dois pontos para setar o tipo de variável
+
+        return $this->ano;
+    }
+
+    public function setAno($ano){
+
+        $this->ano = $ano;
+    }
+
+    public function exibir(){
+
+        return array(
+
+            "modelo"=>$this->getModelo(),
+            "motor"=>$this->getMotor(),
+            "ano"=>$this->getAno()
+
+        );
+
+    }
+
+}
+
+$gol = new Carro();
+$gol->setModelo("Gol GT");
+$gol->setMotor("1.6");
+$gol->setAno("2017");
+
+print_r($gol->exibir());
+?>
