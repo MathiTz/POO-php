b349f7599334d55e7925325dc34f4fb41fd4b32f
mathitz <matheusalves789@gmail.com>
2018-09-27 11:47:08 -0300

    Uso de Método construtor

---
+++ exemplo-04.php
@@ -0,0 +1,39 @@
+<?php
+// método construtor = método mágico, dois underlines
+class Endereco{
+    private $logradouro;
+    private $numero;
+    private $cidade;
+
+    public function __construct($a, $b, $c)
+    {
+        $this->logradouro = $a;
+        $this->numero = $b;
+        $this->cidade = $c;
+    }
+    // para matar variáveis ou sessão de banco de dados
+    /*public function __destruct()
+    {
+        var_dump("DESTRUIR");
+    }*/
+
+    public function __toString()
+    {
+        return $this->logradouro.", ".$this->numero." - ".$this->cidade;
+    }
+
+
+}
+
+
+$meuEndereco = new Endereco("Rua Ademar Saraiva Leão", "123", "Santos");
+
+echo $meuEndereco;
+
+/*var_dump($meuEndereco);
+
+unset($meuEndereco);*/
+
+
+
+?>
